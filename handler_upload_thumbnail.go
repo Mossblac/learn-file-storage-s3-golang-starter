a62295645c68f1a4714efa27b8d1f88a19bc2fc5
Mossblac <Murphyrmurphy2000@gmail.com>
2025-08-27 17:20:36 -0600

    caching using unique URL method

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/base64"
 	"fmt"
 	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -43,20 +48,6 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	defer file.Close()
 
-	mediatype := header.Header.Get("Content-Type")
-	if mediatype == "" {
-		respondWithError(w, http.StatusBadRequest, "missing content-type for thumbnail", nil)
-		return
-	}
-
-	data, err := io.ReadAll(file)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Unable to read multifile", err)
-		return
-	}
-
-	b64data := base64.StdEncoding.EncodeToString(data)
-
 	video, err := cfg.db.GetVideo(videoID)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "no video found in database", err)
@@ -68,11 +59,50 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	url := fmt.Sprintf("data:%s;base64,%s", mediatype, b64data)
+	ct := header.Header.Get("Content-Type")
+	mediatype, _, err := mime.ParseMediaType(ct)
+	if mediatype != "image/jpeg" && mediatype != "image/png" {
+		respondWithError(w, http.StatusUnsupportedMediaType, "unsupported media type", err)
+		return
+	}
+	parts := strings.Split(mediatype, "/")
+	if len(parts) != 2 {
+		respondWithError(w, http.StatusBadRequest, "invalid content-type", nil)
+		return
+	}
+	ext := parts[1]
+
+	var fileString string
+
+	key := make([]byte, 32)
+	rand.Read(key)
+	randString := base64.RawURLEncoding.EncodeToString(key)
+
+	if cfg.assetsRoot == "" {
+		respondWithError(w, http.StatusBadRequest, "missing assetsRoot", nil)
+		return
+	} else {
+		fileString = filepath.Join(cfg.assetsRoot, randString+"."+ext)
+	}
+
+	newfile, err := os.Create(fileString)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "file not created", err)
+		return
+	}
+
+	defer newfile.Close()
+
+	if _, err := io.Copy(newfile, file); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to save to file", err)
+		return
+	}
+
+	url := fmt.Sprintf("http://localhost:%v/assets/%v.%v", cfg.port, randString, ext)
 	video.ThumbnailURL = &url
 	err = cfg.db.UpdateVideo(video)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "unable to update database", err)
+		respondWithError(w, http.StatusInternalServerError, "unable to update video", err)
 		return
 	}
 
